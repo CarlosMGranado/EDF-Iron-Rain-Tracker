fa4ae3843b99c36cd54089ecb8bfd61ba8e64cd9
CarlosMGranado <141454611+CarlosMGranado@users.noreply.github.com>
2026-01-03 22:58:33 +0000

    FEAT: Add weapons info

--- lib/catalog.ts
+++ lib/catalog.ts
@@ -1,11 +1,13 @@
 import assaultRifles from "../data/collections/weapons_assault_rifles.json";
 import shotguns from "../data/collections/weapons_shotguns.json";
+import minigun from "../data/collections/weapons_minigun.json";
 import sniperRifles from "../data/collections/weapons_sniper_rifles.json";
 import rocketLaunchers from "../data/collections/weapons_rocket_launchers.json";
 import missileLaunchers from "../data/collections/weapons_missile_launchers.json";
 import laserRifles from "../data/collections/weapons_laser_rifles.json";
 import grenadeLaunchers from "../data/collections/weapons_grenade_launchers.json";
 import swords from "../data/collections/weapons_swords.json";
+import laserCannon from "../data/collections/weapons_laser_cannon.json";
 import specialAndHeavy from "../data/collections/weapons_special_and_heavy.json";
 import orbital from "../data/collections/weapons_orbital.json";
 import revivalAndVehicles from "../data/collections/items_revival_and_vehicles.json";
@@ -24,6 +26,8 @@ export const COLLECTIONS: Collection[] = [
   swords,
   specialAndHeavy,
   orbital,
+  minigun,
+  laserCannon,
   revivalAndVehicles,
   supportHealCosmetics
 ] as unknown as Collection[];
